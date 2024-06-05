b0bf82400856e20cdfcf4aa71d3aeb9176db3742
Pranav Dave <pranav.dave@schlau-digital.de>
2024-06-05 16:00:32 +0530

    Added string constant files

---
+++ lib/utils/string_constants.dart
@@ -0,0 +1,28 @@
+class ConstantStrings {
+
+  static const onboardingImage = "assets/images/img1.png";
+  static const loginText = "Login";
+  static const emailText = "E-Mail";
+  static const passwordText = "Password";
+  static const confirmPasswordText = "Confirm Password";
+
+  static const enterEmailPassword = "Please Enter Email / Password";
+  static const signInText = "Not a user? Please Sign In";
+
+  static const registerText = "Register";
+  static const pleaseRegisterText = "Please Register";
+
+
+  static const homeScreenText = "Home Screen";
+
+
+  static const senderId = 'senderId';
+  static const senderEmail = "senderEmail";
+  static const receiverId = "receiverId";
+  static const message = "message";
+  static const timestamp = "timestamp";
+  static const chatRoomText = "chat_rooms";
+
+  static const typeMessageFieldText = "Type Here ..";
+
+}
